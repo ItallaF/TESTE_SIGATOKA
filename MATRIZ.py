04684148be3c9135140fdc7cbc8ac5ad9732e8af
ItallaF <87315200+ItallaF@users.noreply.github.com>
2021-07-25 16:10:48 -0300

    Create MATRIZ.py

---
+++ MATRIZ.py
@@ -0,0 +1,13 @@
+# TESTE_SIGATOKA
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 25 15:52:30 2021
+
+@author: Italla
+"""
+
+from PIL import Image
+from numpy import array
+im_1 = Image.open ( r"C:/Users/Italla/Documents/Italla/SI/PROJETOS/SIGATOKA/TESTES/img/folha.jpg" )
+ar = array(im_1)
+ar
